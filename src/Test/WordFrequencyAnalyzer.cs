0118c16d81bae84f98b57a30e704ce36df783b02
kukurt <kudretkrt@gmail.com>
2023-01-31 10:01:58 +0100

    some comments added and dependencies checked

--- src/Test/WordFrequencyAnalyzer.cs
+++ src/Test/WordFrequencyAnalyzer.cs
@@ -4,6 +4,12 @@ namespace Test
 {
     public class WordFrequencyAnalyzer : IWordFrequencyAnalyzer
     {
+        /// <summary>
+        /// You can get frequency count for any word which is in the text
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="word"></param>
+        /// <returns>int</returns>
         public int CalculateFrequencyForWord(string text, string word)
         {
             if(string.IsNullOrWhiteSpace(word)) return 0;
@@ -12,22 +18,45 @@ namespace Test
             return item.Value;
         }
 
+        /// <summary>
+        /// You can get frequency count for any word which is in the text
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="word"></param>
+        /// <returns>Task<int></returns>
         public async Task<int> CalculateFrequencyForWordAsync(string text, string word)
         {
             return await Task.FromResult(CalculateFrequencyForWord(text, word));
         }
 
+        /// <summary>
+        /// You can get highest frequency number in the text
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns>int</returns>
         public int CalculateHighestFrequency(string text)
         {
             var highest = SeperateWordsFromText(text).OrderByDescending(x => x.Value).FirstOrDefault();
             return highest.Value;
         }
 
+
+        /// <summary>
+        /// You can get highest frequency number in the text
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns> Task<int></returns>
         public async Task<int> CalculateHighestFrequencyAsync(string text)
         {
             return await Task.FromResult(CalculateHighestFrequency(text));
         }
 
+        /// <summary>
+        /// You can get list  which is include word and frequency number .If several words have the same frequency, this method return them in ascendant alphabetical order.This list's item count will be until the n number
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
         public IList<IWordFrequency> CalculateMostFrequentNWords(string text, int n)
         {
             var items = SeperateWordsFromText(text)
@@ -38,6 +67,12 @@ namespace Test
             return items.ToList();
         }
 
+        /// <summary>
+        /// You can get list  which is include word and frequency number .If several words have the same frequency, this method return them in ascendant alphabetical order.This list's item count will be until the n number
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
         public async Task<IList<IWordFrequency>> CalculateMostFrequentNWordsAsync(string text, int n)
         {
             return await Task.FromResult(CalculateMostFrequentNWords(text, n));
